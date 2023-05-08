15f397eafa4ad2647a6c091edba282ef59f616d6
diqi122034 <muhammadsalimassahdiqi@student.uir.ac.id>
2023-05-08 19:19:23 +0700

    Add your commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Selamat datang di Aplikasi Input, sebuah platform yang menyediakan solusi dalam mengelola data dengan mudah dan cepat.
+
+Aplikasi Input didirikan dengan tujuan untuk membantu pengguna dalam mengumpulkan, memasukkan, dan mengelola data dengan lebih efisien melalui teknologi digital. Kami menyadari bahwa proses memasukkan data bisa menjadi tugas yang membosankan dan memakan waktu, terutama jika dilakukan secara manual. Oleh karena itu, kami membangun aplikasi input yang sederhana, cepat, dan mudah digunakan.
+
+Dalam Aplikasi Input, kami menyediakan berbagai fitur yang dapat membantu pengguna dalam mengelola data, seperti formulir digital yang dapat dipersonalisasi, input data yang otomatis, dan fitur integrasi dengan aplikasi lainnya. Dengan aplikasi input kami, pengguna dapat menghemat waktu dan tenaga dalam mengumpulkan dan mengelola data.
+
+Kami berkomitmen untuk terus mengembangkan aplikasi input kami dan menambah fitur baru yang bermanfaat bagi pengguna. Kami juga selalu siap untuk menerima masukan dan saran dari pengguna untuk terus meningkatkan kualitas aplikasi kami.
+
+Terima kasih telah memilih Aplikasi Input sebagai platform untuk mengelola data Anda dengan mudah dan cepat. Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau masukan.</p>
+  </div>
+  
+</template>
+
